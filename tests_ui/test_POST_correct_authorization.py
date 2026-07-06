b4d5779da11540ba3b15affe32001a329fd10679
home\asus <danilaqaengineer@gmail.com>
2026-07-06 12:28:19 +0800

    Добавил Аллюр в проект

--- tests_ui/test_POST_correct_authorization.py
+++ tests_ui/test_POST_correct_authorization.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from pages.authorization_page import AuthorizationPage    # Импортирую класс со страницей логинки
+import allure    # прикручиваю Алюр
 
 
 # URL = 'https://www.kvik.ru/'
